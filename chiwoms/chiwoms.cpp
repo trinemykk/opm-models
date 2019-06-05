d8a4e53a814967fee1551f7b4a65ca4d91caac8d
Trine S. Mykkeltvedt <[tmyk@norceresearch.no]>
2019-06-05 11:37:16 +0200

    add the chiwoms sources

--- chiwoms/chiwoms.cpp
+++ chiwoms/chiwoms.cpp
@@ -3,16 +3,32 @@
 
 #include <opm/models/utils/start.hh>
 
-BEGIN_PROPERTIES
+namespace Opm::Properties {
 
-NEW_TYPE_TAG(ChiwomsNcpEcfvProblem, INHERITS_FROM(FlashModel, ChiwomsProblem));
-SET_TAG_PROP(ChiwomsNcpEcfvProblem, SpatialDiscretizationSplice, EcfvDiscretization);
-SET_TAG_PROP(ChiwomsNcpEcfvProblem, LocalLinearizerSplice, AutoDiffLocalLinearizer);
+// NEW_TYPE_TAG(ChiwomsNcpEcfvProblem, INHERITS_FROM(FlashModel, ChiwomsProblem));
+// SET_TAG_PROP(ChiwomsNcpEcfvProblem, SpatialDiscretizationSplice, EcfvDiscretization);
+// SET_TAG_PROP(ChiwomsNcpEcfvProblem, LocalLinearizerSplice, AutoDiffLocalLinearizer);
+namespace TTag {
+    struct ChiwomsNcpEcfvProblem {using InheritsFrom = std::tuple<ChiwomsProblem, FlashModel>;};
 
-END_PROPERTIES
+}
+template <class TypeTag>
+struct SpatialDiscretizationSplice<TypeTag, TTag::ChiwomsNcpEcfvProblem>
+{
+    using type = TTag::EcfvDiscretization;
+};
+template <class TypeTag>
+struct LocalLinearizerSplice<TypeTag, TTag::ChiwomsNcpEcfvProblem>
+{
+    using type = TTag::AutoDiffLocalLinearizer;
+};
+
+
+} // namespace Opm::Properties
 
 int main(int argc, char **argv)
 {
-    typedef TTAG(ChiwomsNcpEcfvProblem) EcfvProblemTypeTag;
+    // typedef TTAG(ChiwomsNcpEcfvProblem) EcfvProblemTypeTag;
+    using EcfvProblemTypeTag = Opm::Properties::TTag::ChiwomsNcpEcfvProblem;
     return Opm::start<EcfvProblemTypeTag>(argc, argv);
 }
