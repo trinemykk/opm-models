6b5af0ebb206c23ce7e56bb992ea3c84e94e1aec
Svenn Tveit <svtv@norceresearch.no>
2021-09-09 21:29:04 +0200

    Store K and L in intensive quantities cache.
    thermodynamicHint does not seem to do what it's suppose to.

--- chiwoms/ChiFlash.hpp
+++ chiwoms/ChiFlash.hpp
@@ -84,11 +84,10 @@ public:
      * \brief Calculates the fluid state from the global mole fractions of the components and the phase pressures
      *
      */
-    template <class FluidState, class Problem>
+    template <class FluidState>
     static void solve(FluidState& fluidState,
                       const Dune::FieldVector<typename FluidState::Scalar, numComponents>& globalComposition,
                       int spatialIdx,
-                      const Problem& problem,
                       int verbosity,
                       std::string twoPhaseMethod,
                       Scalar tolerance)
@@ -113,10 +112,10 @@ public:
         //K and L from previous timestep (wilson and -1 initially)
         ComponentVector K;
         for(int compIdx = 0; compIdx < numComponents; ++compIdx) {
-            K[compIdx] = problem.getKvalue(compIdx, spatialIdx);
+            K[compIdx] = fluidState.K(compIdx);
         }
         InputEval L;
-        L = problem.getLvalue(spatialIdx);
+        L = fluidState.L(0);
 
         // Print header
         if (verbosity >= 1) {
@@ -194,9 +193,9 @@ public:
 
         //Update L and K to the problem for the next flash
         for (int compIdx = 0; compIdx < numComponents; ++compIdx){
-            problem.setKvalue(compIdx,spatialIdx, Opm::getValue(K[compIdx]));
+            fluidState.setKvalue(compIdx, K[compIdx]);
         }
-        problem.setLvalue(spatialIdx, Opm::getValue(L));
+        fluidState.setLvalue(L);
 
 
         // Print saturation
